5c86f15ce41861c3d97e3015e913cc105ba85a7f
Chukwudi Ossai <chuksxceed365@gmail.com>
2019-03-31 21:54:40 +0100

    Code clean up

--- components/TodoItems.js
+++ components/TodoItems.js
@@ -9,7 +9,7 @@ class TodoItems extends React.Component {
         return <li key={task.timeOfInput}>{task.userInput}<span className="delete-button" onClick={() => this.props.deleteTask(task.timeOfInput)}>x</span></li> 
     }
     render() {
-        const tasksInputList = this.props.inputs;
+        const tasksInputList = [] && this.props.inputs;
         const tasksList = tasksInputList.map(this.createTasks);
         return (
             <div className="todoItemContainer">
