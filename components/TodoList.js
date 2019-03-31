c5853143201c036b4f5a7645f9e0fecb630c3d24
Chukwudi Ossai <chuksxceed365@gmail.com>
2019-03-31 08:59:23 +0100

    Add style configuration to app

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,13 +1,34 @@
 import TodoItems from "./TodoItems";
+import Button from '@material-ui/core/Button';
+import '../style.css'
 
 class TodoList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            itemsList: []
+        }
+    }
+
+    addItems($event) {
+        if(this._inputElement.value != '') {
+            const newTodo = {
+                text: this._inputElement.value,
+                key: Date.now() 
+            }
+        }
+
+        const newTodos = this.state.concat(newItem)
+
+        this.setState({items: newTodos})
+    }
     render() {
         return (
-            <div>
+            <div className="container">
                 <div className="header">
                     <form>
-                        <label>Enter Todo:</label>
-                        <input type="text" />
+                        <input type="text" placeholder="Enter Todo"/>
+                        <Button type="submit" variant="contained" color="primary">Add</Button>
                     </form>
                 </div>
                 <TodoItems />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
 import TodoList from '../components/TodoList';
+import App, { Container } from "next/app";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Head from 'next/head';
+import Header from '../components/Layout/Header';
 
-export default () => <TodoList />
+export default () => {
+    return (
+        <Container>
+            <Head>
+                <title>Todo List App</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width shrink-to-fit=no" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+            </Head>
+            <CssBaseline />
+            <Header />
+            <TodoList />
+      </Container>
+    )
+}
